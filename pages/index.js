ad49a7d9b0b8146993a40dfa7b3a52eab7b5f6e3
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-06 23:42:28 -0500

    Update index.js
    three cross jpeg

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
         </h1>
       <div className={styles.hero}>
        <Image 
-                                src="/mark.jpg"
+                                src="/three_crosses_bg.jpeg"
                                 alt="Jesus"
                                 height={900}
                                 width={2000}
