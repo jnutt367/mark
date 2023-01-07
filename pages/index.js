54b31541cb6ed935bd85ca6e1e73e391c4699c3a
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-06 22:45:26 -0500

    Update index.js
    aded mark image

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
         </h1>
       <div className={styles.hero}>
        <Image 
-                                src="/mark.jpg"
+                                src="/44-Mark.jpeg"
                                 alt="Jesus"
                                 height={900}
                                 width={2000}
