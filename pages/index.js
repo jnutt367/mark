93a90ee1d39fbfefb7829307549fb278758b64f3
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-08 14:13:05 -0500

    Update index.js
    gospel-of-saint-mark.jpeg to hero

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
         </h1>
       <div className={styles.hero}>
        <Image 
-                                src="/mark.jpg"
+                                src="/gospel-of-saint-mark.jpeg"
                                 alt="Jesus"
                                 height={900}
                                 width={2000}
