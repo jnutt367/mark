89e89c0e5e7f084aad5f2d6015a1aebde17b9667
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-13 22:48:34 -0500

    Update index.js
    The Book Of

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          <a href="https://en.wikipedia.org/wiki/Mark"> Mark</a>
+          <a href="https://en.wikipedia.org/wiki/Mark">The Book Of</a>
         </h1>
       <div className={styles.hero}>
        <Image 
