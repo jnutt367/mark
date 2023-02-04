d93f1fd3fb0d05f788dc4a7fefa3b905d9217a0c
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-02-04 14:47:43 -0500

    Update index.js
    MARK

--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,16 @@ export default function Home() {
 
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-       MARK
-        </h1>
+       
       <div className={styles.hero}>
+            <h2 className={styles.holy}>THE HOLY</h2>
+                <h1 className={styles.bible}>
+                    <a href="https://the-holy-bible.vercel.app/" target="_blank">BIBLE</a>
+                </h1>
+        
+        <h1 className={styles.title}>
+        MARK
+        </h1>
          <div  className={styles.image}>
        <Image 
                                 src="/illustration-jesus-baptized-by-john-baptist-biblical-series_29028-345.webp"
