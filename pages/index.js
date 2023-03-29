ed5f88dc05ded27ba13fdd404d54c5a90fbec458
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-03-28 22:46:10 -0400

    Update index.js
    import YouTube from "react-youtube";

--- pages/index.js
+++ pages/index.js
@@ -48,7 +48,8 @@ export default function Home() {
 
          <a href="https://gofund.me/a2b590af">
              <p className={styles.donate}>
-               SUPPORT
+               Support <br />
+                  Creator
                </p>
        </a>
            
