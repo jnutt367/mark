e7d14bd46606a5a358aa65e4a20641b8c28697bc
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-04-12 16:12:12 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,13 +27,13 @@ export default function Home() {
         MARK
         </h1>
          <div  className={styles.image}>
-    /*  <Image 
+                          <Image 
                                 src="/BasicJitteryFrogmouth-size_restricted.gif"
                                 alt="the Last supper"
                                  height={900}
                                 width={2000}
                                 priority
-                              /> */
+                              /> 
            
           <iframe width="100%" height="175" src="https://www.youtube.com/embed/zr54VtQNbgg?autoplay=1&mute=1" title="YouTube video player" frameborder="5" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
            </div>
