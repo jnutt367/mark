b31396a131c25838fe49f90ad179c5c5c07f4ece
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-04-12 13:31:04 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
                                 priority
                               /> */
            
-           <iframe width="560" height="315" src="https://www.youtube.com/embed/EoSMmUAusik" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+           <iframe width="100%" height="315" src="https://www.youtube.com/embed/EoSMmUAusik" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
            </div>
               <div className={styles.nav}>               
        <a href="https://matthew-coral.vercel.app/">
