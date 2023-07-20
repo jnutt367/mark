5f608379ea7dd755497d83dc1014023478a45a6d
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-07-19 22:31:05 -0400

    Update index.js
    home.webp

--- pages/index.js
+++ pages/index.js
@@ -56,8 +56,8 @@ export default function Home() {
 
          <a href="https://the-word-of-god.vercel.app/">
               <Image className={styles.arrow}
-                                src="/upward_pointing.webp"
-                                alt="youtube-icon"
+                                src="/home.webp"
+                                alt="home"
                                 height={75}
                                 width={90}
                                 priority
