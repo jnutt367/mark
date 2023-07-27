39b7aa1ccdc31d503fd3b08fa2e7170db5800346
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-07-26 21:17:59 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1111,8 +1111,7 @@ The Burial of Jesus
      
 
              <div className={styles.iframe}>
-         
-      </div> 
+         <iframe width="100%" height="315" src="https://www.youtube.com/embed/urlvnxCaL00?autoplay=1&mute=1" title="YouTube video player" frameborder="5" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>      </div> 
   
   </div>
   
